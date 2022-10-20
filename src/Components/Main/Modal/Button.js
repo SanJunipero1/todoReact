5289eb38c403eef1fc4ab19aaa9752de2c920510
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2022-10-20 16:00:45 +0200

    first attempt to toggle

--- src/Components/Main/Modal/Button.js
+++ src/Components/Main/Modal/Button.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-
-export default function Button({name}){
-    return(
-      <button>{name}</button>
-    )
+export default function Button({ name, onClick, id, type }) {
+  return (
+    <button onClick={onClick} name={name} id={id} type={type}>
+      {name}
+    </button>
+  );
 }
--- src/Components/Main/Modal/Form.js
+++ src/Components/Main/Modal/Form.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "./Button";
 
 export default function Form() {
+  const input = useRef(null);
+
   function handleChange(event) {
     console.log(event.target.value);
+  }
+
+  function submitToDo() {
+    console.log("Submitted!");
+  }
+  function cancelToDo() {
+    //input.value = "";
+    console.log("cancelBtn works!");
   }
 
   return (
     <form method="dialog">
       <input
+        ref={input}
         onChange={handleChange}
         id="todoTitle"
         type="text"
         placeholder="What's to do?"
       />
-      <Button id="submit" type="submit" name={"Submit"} />
-      <Button id="cancel" name={"Cancel"} />
+      <Button onCLick={submitToDo} id="submit" type="submit" name={"Submit"} />
+      <Button onClick={cancelToDo} id="cancel" name={"Cancel"} />
     </form>
   );
 }
